bf5b737c5fc225bf99f374fcd51579427770b94e
Cristian Traina <crissstian96@gmail.com>
2020-04-15 17:50:12 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+declare var cordova;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  isMicrophoneActive = false;
+
+  constructor(private cd: ChangeDetectorRef) {
+  }
+  
+  ngOnInit() {
+  }
+
+  callCordova() {
+    const audioBytecodeBufferArray = [];
+    cordova.plugins.AudioVolume.coolMethod('Hello world', rawData => {
+      if (rawData !== null) {
+        audioBytecodeBufferArray.push(rawData);
+        this.isMicrophoneActive = true;
+        this.cd.detectChanges();
+        setTimeout(() => {
+          this.isMicrophoneActive = false;
+          this.cd.detectChanges();
+        }, 100);
+      }
+    }, y => console.error(y));
+  }
 
 }
